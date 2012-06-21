97b06058a010814dc41e395399b9623c3a492eec
Alex Stanev <alex@stanev.org>
2012-06-21 10:55:10 +0300

    quickly saved

--- winconn/WinconnWindow.py
+++ winconn/WinconnWindow.py
@@ -17,7 +17,24 @@ from winconn.PreferencesWinconnDialog import PreferencesWinconnDialog
 
 # See winconn_lib.Window.py for more details about how this class works
 class WinconnWindow(Window):
-    __gtype_name__ = "WinconnWindow"
+    __gtype_name__ = 'WinconnWindow'
+    
+    def emptyApp():
+        self.ui.eName.set_text('')
+        self.ui.eApp.set_text('')
+        self.ui.eSrv.set_text('')
+        self.ui.ePort.set_text('3389')
+        self.ui.eUser.set_text('')
+        self.ui.ePass.set_text('')
+        self.ui.eDomain.set_text('')
+        self.ui.eFolder.set_text('')
+        self.ui.cbComp.set_active(False)
+        self.ui.cbClip.set_active(False)
+        self.ui.cbSound.set_active(False)
+        self.ui.cbRFX.set_active(False)
+    
+    def createAppList():
+        return []
     
     def finish_initializing(self, builder): # pylint: disable=E1002
         """Set up the main window"""
@@ -25,6 +42,36 @@ class WinconnWindow(Window):
 
         self.AboutDialog = AboutWinconnDialog
         self.PreferencesDialog = PreferencesWinconnDialog
+        
+        # FIXME do it with iterator
+        cell = Gtk.CellRendererText()
+        col = self.ui.tvApps.get_column(0)        
+        col.pack_start(cell, True)
+        col.add_attribute (cell, 'text', 0)
+        
+        col = self.ui.tvApps.get_column(1)
+        col.pack_start(cell, True)
+        col.add_attribute (cell, 'text', 1)
 
-        # Code for other initialization actions should be added here.
+        col = self.ui.tvApps.get_column(2)
+        col.pack_start(cell, True)
+        col.add_attribute (cell, 'text', 2)
 
+
+    def tbNew_clicked_cb(self, widget):
+        self.emptyApp
+        self.set_focus(self.ui.eName)
+        self.ui.notebook.set_current_page(1)
+        
+    def tbExec_clicked_cb(self, widget, data=None):
+        if self.ui.tsApp.count_selected_rows() == 0:
+            self.ui.lStatus.set_text('No application selected')
+
+    def bCancel_clicked(self, widget):
+        self.ui.notebook.set_current_page(0)
+        self.emptyApp
+
+    def bSave_clicked(self, widget, data=None):
+        if self.ui.tsApp.count_selected_rows() == 0:
+            # TODO create new savefile
+            self.ui.lsApps.append(["aman","zaman","boza"])
