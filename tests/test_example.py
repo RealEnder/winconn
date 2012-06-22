476c7c140eccf85bbaf58b57f141bcf18e6803df
Alex Stanev <alex@stanev.org>
2012-06-22 17:27:17 +0300

    License GPLv3

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
-### BEGIN LICENSE
-# This file is in the public domain
-### END LICENSE
+'''
+This file is part of WinConn.
+
+WinConn is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+WinConn is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WinConn.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 import sys
 import os.path
