a8f6e53f01598fa57cffe06d0cb9602bb56e0e9e
Alex Stanev <alex@stanev.org>
2012-06-26 17:51:18 +0300

    add dbus service to handle single instance and new app creation

--- winconn/__init__.py
+++ winconn/__init__.py
@@ -21,11 +21,27 @@ from gettext import gettext as _
 gettext.textdomain('winconn')
 
 from gi.repository import Gtk, GObject # pylint: disable=E0611
+import dbus, dbus.service, dbus.glib
 
 from winconn import WinconnWindow
 from winconn_lib import set_up_logging, get_version
 
 GObject.threads_init()
+
+class WinconnService(dbus.service.Object):
+    def __init__(self, app):
+        self.app = app
+        bus_name = dbus.service.BusName('org.stanev.winconn', bus = dbus.SessionBus())
+        dbus.service.Object.__init__(self, bus_name, '/org/stanev/winconn')
+
+    @dbus.service.method(dbus_interface='org.stanev.winconn')
+    def show_window(self):
+        self.app.present()
+
+    @dbus.service.method(dbus_interface='org.stanev.winconn')
+    def new_app(self):
+        self.app.tbNew_clicked(None)
+        self.app.present()
 
 def parse_options():
     """Support for command line options"""
@@ -35,7 +51,7 @@ def parse_options():
         "-v", "--verbose", action="count", dest="verbose",
         help=_("Show debug messages (-vv debugs winconn_lib also)"))
     parser.add_argument(
-        "-n", "--new",
+        "-n", "--new", action='store_true', default=False, dest='new',
         help=_("Create new application connection"))
     parser.add_argument(
         "-e", "--execute", dest="FILE",
@@ -43,12 +59,26 @@ def parse_options():
     args = parser.parse_args()
 
     set_up_logging(args)
+    
+    return args
 
 def main():
     'constructor for your class instances'
-    parse_options()
-
-    # Run the application.
-    window = WinconnWindow.WinconnWindow()
-    window.show()
-    Gtk.main()
+    args = parse_options()
+    print(args)
+    
+    # check if WinConn is running and pass params to it
+    if dbus.SessionBus().request_name("org.stanev.winconn") != dbus.bus.REQUEST_NAME_REPLY_PRIMARY_OWNER:
+        if args.new:
+            method = dbus.SessionBus().get_object("org.stanev.winconn", "/org/stanev/winconn").get_dbus_method("new_app")
+        else:
+            method = dbus.SessionBus().get_object("org.stanev.winconn", "/org/stanev/winconn").get_dbus_method("show_window")
+        method()
+    else:
+        # Run the application
+        window = WinconnWindow.WinconnWindow()
+        service = WinconnService(window)
+        if args.new:
+            window.tbNew_clicked(None)
+        window.show()
+        Gtk.main()
