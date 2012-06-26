6aa3b8f3bb4beaf6aa1de70e5141055d6e88bbb8
Alex Stanev <alex@stanev.org>
2012-06-26 13:03:07 +0300

    some quickly automagics

--- winconn_lib/helpers.py
+++ winconn_lib/helpers.py
@@ -1,22 +1,17 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
-'''
-This file is part of WinConn http://stanev.org/winconn
-Copyright (C) 2012 Alex Stanev <alex@stanev.org>
-
-WinConn is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
-
-WinConn is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with WinConn.  If not, see <http://www.gnu.org/licenses/>.
-'''
+# Copyright (C) 2012 Alex Stanev <alex@stanev.org>
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3, as published 
+# by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
 """Helpers for an Ubuntu application."""
