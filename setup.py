3b39e0caa6fa4c5de112fd7ee08cebe3e7f69fb2
Alex Stanev <alex@stanev.org>
2012-06-29 23:20:35 +0300

    commit before release

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='winconn',
-    version='0.2.3',
+    version='0.2.4',
     license='GPL-3',
     author='Alex Stanev',
     author_email='alex@stanev.org',
